386e19d30e9b0434c690b1bd8a4aad244595aeef
Geoffrey Ihsan <geoffreyihsan@geoffrey-4.local>
2023-09-05 16:46:42 +0700

    Test deployment on 5th September 2023

--- components/FeatureWell/FeatureWell.js
+++ components/FeatureWell/FeatureWell.js
@@ -5,6 +5,8 @@ import { useMediaQuery } from 'react-responsive'
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react'
 import { Autoplay, Pagination, Navigation } from 'swiper'
+import Div100vh from 'react-div-100vh'
+
 
 // Import Swiper styles
 import 'swiper/css'
@@ -37,24 +39,27 @@ export default function FeatureWell({ featureWells }) {
 
   return (
     <>
-      <Swiper
-        spaceBetween={30}
-        centeredSlides={true}
-        autoplay={{
-          delay: 2500,
-          disableOnInteraction: false,
-        }}
-        pagination={{
-          clickable: true,
-        }}
-        navigation={true}
-        modules={[Autoplay, Pagination, Navigation]}
-        onAutoplayTimeLeft={onAutoplayTimeLeft}
-        className="mySwiper"
-      >
-        {featureWells.map((featureWell, index) => (
-          <SwiperSlide key={index}>
-            <div className={cx('component')}>
+      <Div100vh>
+        <Swiper
+          spaceBetween={30}
+          centeredSlides={true}
+          autoplay={{
+            delay: 10000,
+            disableOnInteraction: true,
+          }}
+          pagination={{
+            clickable: true,
+          }}
+          navigation={{
+            prevEl: '.swiper-custom-button-prev',
+            nextEl: '.swiper-custom-button-next',
+          }}
+          modules={[Autoplay, Pagination, Navigation]}
+          onAutoplayTimeLeft={onAutoplayTimeLeft}
+          className="fw-swiper-wrapper"
+        >
+          {featureWells?.map((featureWell, index) => (
+            <SwiperSlide key={index}>
               {featureWell.type === 'image' && (
                 <a href={featureWell.url}>
                   {isDesktop && <img src={featureWell.desktopSrc} />}
@@ -79,46 +84,94 @@ export default function FeatureWell({ featureWells }) {
                   </div>
                 </a>
               )}
-            </div>
-          </SwiperSlide>
-        ))}
-        <div className="autoplay-progress" slot="container-end">
-          <svg viewBox="0 0 48 48" ref={progressCircle}>
-            <circle cx="24" cy="24" r="20"></circle>
-          </svg>
-          <span ref={progressContent}></span>
-        </div>
-        <div
-                      className={cx('chevron-wrapper', {
-                        maximized: IsMaximized,
-                      })}
+              <div
+                className={cx('chevron-wrapper', {
+                  maximized: IsMaximized,
+                })}
+              >
+                <a href="#snapStart">
+                  <svg
+                    version="1.0"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="512.000000pt"
+                    height="512.000000pt"
+                    viewBox="0 0 512.000000 512.000000"
+                    preserveAspectRatio="xMidYMid meet"
+                  >
+                    <g
+                      transform="translate(0.000000,512.000000) scale(0.100000,-0.100000)"
+                      fill="#ffffff"
+                      stroke="none"
                     >
-                      <a href="#snapStart">
-                        <svg
-                          version="1.0"
-                          xmlns="http://www.w3.org/2000/svg"
-                          width="512.000000pt"
-                          height="512.000000pt"
-                          viewBox="0 0 512.000000 512.000000"
-                          preserveAspectRatio="xMidYMid meet"
-                        >
-                          <g
-                            transform="translate(0.000000,512.000000) scale(0.100000,-0.100000)"
-                            fill="#ffffff"
-                            stroke="none"
-                          >
-                            <path
-                              d="M1387 5110 c-243 -62 -373 -329 -272 -560 27 -62 77 -114 989 -1027
+                      <path
+                        d="M1387 5110 c-243 -62 -373 -329 -272 -560 27 -62 77 -114 989 -1027
     l961 -963 -961 -963 c-912 -913 -962 -965 -989 -1027 -40 -91 -46 -200 -15
     -289 39 -117 106 -191 220 -245 59 -28 74 -31 160 -30 74 0 108 5 155 23 58
     22 106 70 1198 1160 1304 1302 1202 1185 1202 1371 0 186 102 69 -1202 1371
     -1102 1101 -1140 1137 -1198 1159 -67 25 -189 34 -248 20z"
-                            />
-                          </g>
-                        </svg>
-                      </a>
-                    </div>
-      </Swiper>
+                      />
+                    </g>
+                  </svg>
+                </a>
+              </div>
+            </SwiperSlide>
+          ))}
+          <div className="swiper-custom-button-prev">
+            <svg
+              version="1.0"
+              xmlns="http://www.w3.org/2000/svg"
+              width="512.000000pt"
+              height="512.000000pt"
+              viewBox="0 0 512.000000 512.000000"
+              preserveAspectRatio="xMidYMid meet"
+            >
+              <g
+                transform="translate(0.000000,512.000000) scale(0.100000,-0.100000)"
+                fill="#FFFFFF"
+                stroke="none"
+              >
+                <path
+                  d="M1387 5110 c-243 -62 -373 -329 -272 -560 27 -62 77 -114 989 -1027
+l961 -963 -961 -963 c-912 -913 -962 -965 -989 -1027 -40 -91 -46 -200 -15
+-289 39 -117 106 -191 220 -245 59 -28 74 -31 160 -30 74 0 108 5 155 23 58
+22 106 70 1198 1160 1304 1302 1202 1185 1202 1371 0 186 102 69 -1202 1371
+-1102 1101 -1140 1137 -1198 1159 -67 25 -189 34 -248 20z"
+                />
+              </g>
+            </svg>
+          </div>
+          <div className="swiper-custom-button-next">
+            <svg
+              version="1.0"
+              xmlns="http://www.w3.org/2000/svg"
+              width="512.000000pt"
+              height="512.000000pt"
+              viewBox="0 0 512.000000 512.000000"
+              preserveAspectRatio="xMidYMid meet"
+            >
+              <g
+                transform="translate(0.000000,512.000000) scale(0.100000,-0.100000)"
+                fill="#FFFFFF"
+                stroke="none"
+              >
+                <path
+                  d="M1387 5110 c-243 -62 -373 -329 -272 -560 27 -62 77 -114 989 -1027
+l961 -963 -961 -963 c-912 -913 -962 -965 -989 -1027 -40 -91 -46 -200 -15
+-289 39 -117 106 -191 220 -245 59 -28 74 -31 160 -30 74 0 108 5 155 23 58
+22 106 70 1198 1160 1304 1302 1202 1185 1202 1371 0 186 102 69 -1202 1371
+-1102 1101 -1140 1137 -1198 1159 -67 25 -189 34 -248 20z"
+                />
+              </g>
+            </svg>
+          </div>
+          <div className="autoplay-progress" slot="container-end">
+            <svg viewBox="0 0 48 48" ref={progressCircle}>
+              <circle cx="24" cy="24" r="20"></circle>
+            </svg>
+            <span ref={progressContent}></span>
+          </div>
+        </Swiper>
+      </Div100vh>
     </>
   )
 }
