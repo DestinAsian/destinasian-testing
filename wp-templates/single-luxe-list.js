b88578c459372f8fc81d597d16453faafdc4d114
Geoffrey Ihsan <geoffreyihsan@geoffrey-2.local>
2023-10-04 17:42:49 +0700

    Test deployment on 4th October 2023

--- wp-templates/single-luxe-list.js
+++ wp-templates/single-luxe-list.js
@@ -162,36 +162,36 @@ export default function SingleLuxeList(props) {
         />
       )}
       {parent != null && (
-        <Main>
-          <>
-            {/* {'hotel'} */}
-            <SingleLLContainer>
-              <div className="sm:relative sm:left-[50vw] sm:flex sm:w-[50vw] sm:flex-col	sm:justify-between">
-                {/* First wrapper */}
-                <div>
-                  <SingleLLFeaturedImage
-                    mainLogo={parent?.node?.luxeListLogo?.mainLogo}
-                    secondaryLogo={parent?.node?.luxeListLogo?.secondaryLogo}
-                    databaseId={parent?.node?.databaseId}
-                    uri={parent?.node?.uri}
-                  />
+          <Main>
+            <>
+              {/* {'hotel'} */}
+              <SingleLLContainer>
+                <div className="sm:fixed sm:left-[50vw] sm:flex sm:h-[calc(100vh-4.5rem)] sm:w-[50vw] sm:flex-col	sm:justify-between">
+                  {/* First wrapper */}
+                  <div>
+                    <SingleLLFeaturedImage
+                      mainLogo={parent?.node?.luxeListLogo?.mainLogo}
+                      secondaryLogo={parent?.node?.luxeListLogo?.secondaryLogo}
+                      databaseId={parent?.node?.databaseId}
+                      uri={parent?.node?.uri}
+                    />
+                  </div>
+                  {/* Second wrapper */}
+                  <div>
+                    <SingleLLEntryHeader
+                      title={title}
+                      category={categories?.edges[0]?.node?.name}
+                    />
+                    <ContentWrapperLL
+                      content={content}
+                      images={images}
+                      databaseId={databaseId}
+                    />
+                  </div>
                 </div>
-                {/* Second wrapper */}
-                <div>
-                  <SingleLLEntryHeader
-                    title={title}
-                    category={categories?.edges[0]?.node?.name}
-                  />
-                  <ContentWrapperLL
-                    content={content}
-                    images={images}
-                    databaseId={databaseId}
-                  />
-                </div>
-              </div>
-            </SingleLLContainer>
-          </>
-        </Main>
+              </SingleLLContainer>
+            </>
+          </Main>
       )}
       {/* <Footer /> */}
     </>
