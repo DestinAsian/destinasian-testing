6107ffd0b88b1a5f941b261212866232e2246422
Geoffrey Ihsan <geoffreyihsan@geoffrey-3.local>
2023-10-17 14:09:51 +0700

    Test deployment on 17th October 2023

--- components/PartnerContent/PartnerContent.js
+++ components/PartnerContent/PartnerContent.js
@@ -2,7 +2,7 @@ import { useQuery } from '@apollo/client'
 import React, { useState, useEffect } from 'react'
 import classNames from 'classnames/bind'
 import styles from './PartnerContent.module.scss'
-import { GetAdvertorialStories } from '../../queries/GetAdvertorialStories'
+import { GetPartnerContent } from '../../queries/GetPartnerContent'
 import { Button, FeaturedImage } from '../../components'
 
 let cx = classNames.bind(styles)
@@ -36,35 +36,54 @@ function truncateText(text) {
 
 export default function PartnerContent({ parentName }) {
   // Initialize a state variable to hold shuffled more reviews
-  const [shuffledPartnerContent, setShuffledPartnerContent] = useState([])
+  const [shuffledAdvContent, setShuffledAdvContent] = useState([])
+  const [shuffledHcContent, setShuffledHcContent] = useState([])
 
   const contentPerPage = 10
 
   // Get Stories
-  const { data, error, loading } = useQuery(GetAdvertorialStories, {
+  const { data, error, loading } = useQuery(GetPartnerContent, {
     variables: {
       first: contentPerPage,
       search: parentName,
+      id: '/honors-circle',
     },
     fetchPolicy: 'network-only',
     nextFetchPolicy: 'cache-and-network',
   })
 
-  console.log(data)
+  // advertorial content
+  const advContent = data?.advertorials?.edges ?? []
 
-  const partnerContent = data?.advertorials?.edges ?? []
+  // honorsCircle content
+  const hcContent = data?.honorsCircles?.edges ?? []
+
+  const hcFrontPage = data?.page
 
   // Use an effect to shuffle more reviews when data changes
+  // Advertorial content
   useEffect(() => {
-    if (partnerContent && partnerContent.length > 0) {
+    if (advContent && advContent.length > 0) {
       // Clone the partnerContent array to avoid modifying the original data
-      const clonedPartnerContent = [...partnerContent]
+      const clonedAdvContent = [...advContent]
       // Shuffle the cloned array
-      const shuffledArray = shuffleArray(clonedPartnerContent)
+      const shuffledArray = shuffleArray(clonedAdvContent)
       // Set the shuffled array in state
-      setShuffledPartnerContent(shuffledArray)
+      setShuffledAdvContent(shuffledArray)
     }
-  }, [partnerContent]) // Trigger shuffling when partnerContent changes
+  }, [advContent]) // Trigger shuffling when partnerContent changes
+
+  // HC content
+  useEffect(() => {
+    if (hcContent && hcContent.length > 0) {
+      // Clone the partnerContent array to avoid modifying the original data
+      const clonedHcContent = [...hcContent]
+      // Shuffle the cloned array
+      const shuffledArray = shuffleArray(clonedHcContent)
+      // Set the shuffled array in state
+      setShuffledHcContent(shuffledArray)
+    }
+  }, [hcContent]) // Trigger shuffling when partnerContent changes
 
   if (error) {
     return <pre>{JSON.stringify(error)}</pre>
@@ -80,51 +99,109 @@ export default function PartnerContent({ parentName }) {
     )
   }
 
-  // Show only the first 3 items from shuffledPartnerContent
-  const firstFiveContent = shuffledPartnerContent.slice(0, 5)
+  // Show only the first 2 items from shuffledAdvContent
+  const firstTwoContent = shuffledAdvContent.slice(0, 2)
+  const firstContent = shuffledAdvContent.slice(0, 1)
+
+  // Show only the first item from shuffledHcContent
+  const firstHcContent = shuffledHcContent.slice(0, 1)
 
   return (
-    <div className={cx('component')}>
-      {firstFiveContent.map((post) => (
-        <>
-          <article className={cx('main-wrapper')}>
-            <div className={cx('left-wrapper')}>
-              {post?.node?.featuredImage && (
-                <a href={post?.node?.uri}>
-                  <div className={cx('content-wrapper-image')}>
-                    <FeaturedImage
-                      image={post?.node?.featuredImage?.node}
-                      layout={'responsive'}
-                      className={cx('image')}
-                    />
-                  </div>
-                </a>
-              )}
-            </div>
-            <div className={cx('right-wrapper')}>
-              <div className={cx('content-wrapper')}>
-                <a href={post?.node?.uri}>
-                  <h2 className={cx('title')}>{post?.node?.title}</h2>
-                </a>
-              </div>
-              {post?.node?.excerpt !== undefined &&
-                post?.node?.excerpt !== null && (
-                  <div className={cx('content-wrapper')}>
-                    <a href={post?.node?.uri}>
-                      <div
-                        className={cx('excerpt', 'truncate-text')} // Add the class here
-                        dangerouslySetInnerHTML={{
-                          __html: truncateText(post?.node?.excerpt),
-                        }}
+    <>
+      {/* Adv Content */}
+      <div className={cx('adv-component')}>
+        {firstTwoContent.map((post) => (
+          <>
+            <article className={cx('main-wrapper')}>
+              <div className={cx('left-wrapper')}>
+                {post?.node?.featuredImage && (
+                  <a href={post?.node?.uri}>
+                    <div className={cx('content-wrapper-image')}>
+                      <FeaturedImage
+                        image={post?.node?.featuredImage?.node}
+                        layout={'responsive'}
+                        className={cx('image')}
                       />
-                    </a>
-                  </div>
+                    </div>
+                  </a>
                 )}
-            </div>
-          </article>
-          <div className={cx('border-bottom')}></div>
-        </>
-      ))}
-    </div>
+              </div>
+              <div className={cx('right-wrapper')}>
+                <div className={cx('content-wrapper')}>
+                  <a href={post?.node?.uri}>
+                    <h2 className={cx('title')}>{post?.node?.title}</h2>
+                  </a>
+                </div>
+                {post?.node?.excerpt !== undefined &&
+                  post?.node?.excerpt !== null && (
+                    <div className={cx('content-wrapper')}>
+                      <a href={post?.node?.uri}>
+                        <div
+                          className={cx('excerpt', 'truncate-text')} // Add the class here
+                          dangerouslySetInnerHTML={{
+                            __html: truncateText(post?.node?.excerpt),
+                          }}
+                        />
+                      </a>
+                    </div>
+                  )}
+              </div>
+            </article>
+            {post?.node?.title === firstContent[0]?.node?.title && (
+              <div className={cx('border-bottom')}></div>
+            )}
+          </>
+        ))}
+      </div>
+      {/* HC Content */}
+      <div className={cx('hc-component')}>
+        {firstHcContent.map((post) => (
+          <>
+            <article className={cx('main-wrapper')}>
+              <div className={cx('left-wrapper')}>
+                {post?.node?.featuredImage && (
+                  <a href={post?.node?.uri}>
+                    <div className={cx('content-wrapper-image')}>
+                      <FeaturedImage
+                        image={post?.node?.featuredImage?.node}
+                        layout={'responsive'}
+                        className={cx('image')}
+                      />
+                    </div>
+                  </a>
+                )}
+              </div>
+              <div className={cx('right-wrapper')}>
+                <div className={cx('hc-content-wrapper')}>
+                  <a href={hcFrontPage?.uri}>
+                    <h2 className={cx('honors-circle')}>
+                      {hcFrontPage?.title}
+                    </h2>
+                  </a>
+                </div>
+                <div className={cx('hc-content-wrapper')}>
+                  <a href={post?.node?.uri}>
+                    <h2 className={cx('hc-title')}>{post?.node?.title}</h2>
+                  </a>
+                </div>
+                {post?.node?.excerpt !== undefined &&
+                  post?.node?.excerpt !== null && (
+                    <div className={cx('hc-content-wrapper')}>
+                      <a href={post?.node?.uri}>
+                        <div
+                          className={cx('hc-excerpt', 'truncate-text')} // Add the class here
+                          dangerouslySetInnerHTML={{
+                            __html: truncateText(post?.node?.excerpt),
+                          }}
+                        />
+                      </a>
+                    </div>
+                  )}
+              </div>
+            </article>
+          </>
+        ))}
+      </div>
+    </>
   )
 }
