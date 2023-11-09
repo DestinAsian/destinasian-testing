b9c3bb16be69f79dc129ca72861a363e8117fbf8
Geoffrey Ihsan <geoffreyihsan@geoffrey-3.local>
2023-11-09 15:26:58 +0700

    Test deployment on 9th November 2023

--- pages/preview.js
+++ pages/preview.js
@@ -1,38 +1,5 @@
-import { getWordPressProps, WordPressTemplate } from '@faustwp/core'
-import { useAuth } from '@faustwp/core'
-import { Button } from '../components'
+import { WordPressTemplate } from "@faustwp/core";
 
 export default function Preview(props) {
-  const { isAuthenticated, isReady, loginUrl } = useAuth()
-
-  if (!isReady) {
-    return (
-      <>
-        <div className="mx-auto my-0 flex max-w-[100vw] justify-center md:max-w-[700px]	">
-          <Button className="gap-x-4	">{'Loading...'}</Button>
-        </div>
-      </>
-    )
-  }
-
-  if (isAuthenticated === true) {
-    return (
-      <>
-        <WordPressTemplate {...props} />
-      </>
-    )
-  }
-
-  return (
-    <>
-      <div className="mx-auto my-0 flex max-w-[100vw] justify-center md:max-w-[700px]	">
-        You need to login first!
-      </div>
-      <div className="mx-auto my-0 flex max-w-[100vw] justify-center md:max-w-[700px]	">
-        <Button className="gap-x-4	">
-          <a href={loginUrl}>Login</a>
-        </Button>
-      </div>
-    </>
-  )
+  return <WordPressTemplate {...props} />;
 }
