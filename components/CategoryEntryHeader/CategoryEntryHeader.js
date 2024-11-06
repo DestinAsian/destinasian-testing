1337184aebdbf5c76879732309cc8c09b9d63f30
WEB 1 <web1@WEBs-iMac.local>
2024-11-06 15:37:06 +0700

    Modified CategoryEntryHeader, LuxuryTravelStories

--- components/CategoryEntryHeader/CategoryEntryHeader.js
+++ components/CategoryEntryHeader/CategoryEntryHeader.js
@@ -43,6 +43,103 @@ export default function CategoryEntryHeader({
     return index
   })
 
+  const swiperComponent = (
+    <>
+      <Swiper
+        spaceBetween={30}
+        effect={'fade'}
+        loop={true}
+        autoplay={{
+          delay: 25000,
+          disableOnInteraction: true,
+        }}
+        pagination={{
+          clickable: true,
+          el: '.swiper-post-custom-pagination',
+          clickable: true,
+          renderBullet: function (index, className) {
+            return `<span key="${menuIndex[index]}" class="${className}"></span>`
+          },
+        }}
+        navigation={{
+          prevEl: '.swiper-custom-button-prev',
+          nextEl: '.swiper-custom-button-next',
+        }}
+        modules={[EffectFade, Autoplay, Pagination, Navigation]}
+        className="post-category-swiper"
+      >
+        {categorySlider?.map((image, index) => (
+          <div key={index} className="post-swiper-slide">
+            {image[0] && (
+              <SwiperSlide key={index}>
+                <Image
+                  src={image[0]}
+                  alt={'Slider Image' + index}
+                  fill
+                  sizes="100%"
+                  priority
+                />
+                {image[1] && (
+                  <figcaption className={'slide-caption'}>
+                    <span className={'caption'}>{image[1]}</span>
+                  </figcaption>
+                )}
+              </SwiperSlide>
+            )}
+          </div>
+        ))}
+        <div className="swiper-custom-button-prev">
+          <svg
+            version="1.0"
+            xmlns="http://www.w3.org/2000/svg"
+            width="512.000000pt"
+            height="512.000000pt"
+            viewBox="0 0 512.000000 512.000000"
+            preserveAspectRatio="xMidYMid meet"
+          >
+            <g
+              transform="translate(0.000000,512.000000) scale(0.100000,-0.100000)"
+              fill="#FFFFFF"
+              stroke="none"
+            >
+              <path
+                d="M1387 5110 c-243 -62 -373 -329 -272 -560 27 -62 77 -114 989 -1027
+l961 -963 -961 -963 c-912 -913 -962 -965 -989 -1027 -40 -91 -46 -200 -15
+-289 39 -117 106 -191 220 -245 59 -28 74 -31 160 -30 74 0 108 5 155 23 58
+22 106 70 1198 1160 1304 1302 1202 1185 1202 1371 0 186 102 69 -1202 1371
+-1102 1101 -1140 1137 -1198 1159 -67 25 -189 34 -248 20z"
+              />
+            </g>
+          </svg>
+        </div>
+        <div className="swiper-custom-button-next">
+          <svg
+            version="1.0"
+            xmlns="http://www.w3.org/2000/svg"
+            width="512.000000pt"
+            height="512.000000pt"
+            viewBox="0 0 512.000000 512.000000"
+            preserveAspectRatio="xMidYMid meet"
+          >
+            <g
+              transform="translate(0.000000,512.000000) scale(0.100000,-0.100000)"
+              fill="#FFFFFF"
+              stroke="none"
+            >
+              <path
+                d="M1387 5110 c-243 -62 -373 -329 -272 -560 27 -62 77 -114 989 -1027
+l961 -963 -961 -963 c-912 -913 -962 -965 -989 -1027 -40 -91 -46 -200 -15
+-289 39 -117 106 -191 220 -245 59 -28 74 -31 160 -30 74 0 108 5 155 23 58
+22 106 70 1198 1160 1304 1302 1202 1185 1202 1371 0 186 102 69 -1202 1371
+-1102 1101 -1140 1137 -1198 1159 -67 25 -189 34 -248 20z"
+              />
+            </g>
+          </svg>
+        </div>
+      </Swiper>
+    </>
+  )
+
   return (
     <div className={cx(['component', className])}>
       {isNormalCategory && (
@@ -64,100 +161,7 @@ export default function CategoryEntryHeader({
                   )}
                   {categorySlider ? (
                     <figure className={cx('image-slider')}>
-                      <Swiper
-                        spaceBetween={30}
-                        effect={'fade'}
-                        loop={true}
-                        autoplay={{
-                          delay: 25000,
-                          disableOnInteraction: true,
-                        }}
-                        pagination={{
-                          clickable: true,
-                          el: '.swiper-post-custom-pagination',
-                          clickable: true,
-                          renderBullet: function (index, className) {
-                            return `<span key="${menuIndex[index]}" class="${className}"></span>`
-                          },
-                        }}
-                        navigation={{
-                          prevEl: '.swiper-custom-button-prev',
-                          nextEl: '.swiper-custom-button-next',
-                        }}
-                        modules={[EffectFade, Autoplay, Pagination, Navigation]}
-                        className="post-category-swiper"
-                      >
-                        {categorySlider?.map((image, index) => (
-                          <div className="post-swiper-slide">
-                            {image[0] && (
-                              <SwiperSlide key={index}>
-                                <Image
-                                  src={image[0]}
-                                  alt={'Slider Image' + index}
-                                  fill
-                                  sizes="100%"
-                                  priority
-                                />
-                                {image[1] && (
-                                  <figcaption className={'slide-caption'}>
-                                    <span className={'caption'}>
-                                      {image[1]}
-                                    </span>
-                                  </figcaption>
-                                )}
-                              </SwiperSlide>
-                            )}
-                          </div>
-                        ))}
-                        <div className="swiper-custom-button-prev">
-                          <svg
-                            version="1.0"
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="512.000000pt"
-                            height="512.000000pt"
-                            viewBox="0 0 512.000000 512.000000"
-                            preserveAspectRatio="xMidYMid meet"
-                          >
-                            <g
-                              transform="translate(0.000000,512.000000) scale(0.100000,-0.100000)"
-                              fill="#FFFFFF"
-                              stroke="none"
-                            >
-                              <path
-                                d="M1387 5110 c-243 -62 -373 -329 -272 -560 27 -62 77 -114 989 -1027
-l961 -963 -961 -963 c-912 -913 -962 -965 -989 -1027 -40 -91 -46 -200 -15
--289 39 -117 106 -191 220 -245 59 -28 74 -31 160 -30 74 0 108 5 155 23 58
-22 106 70 1198 1160 1304 1302 1202 1185 1202 1371 0 186 102 69 -1202 1371
--1102 1101 -1140 1137 -1198 1159 -67 25 -189 34 -248 20z"
-                              />
-                            </g>
-                          </svg>
-                        </div>
-                        <div className="swiper-custom-button-next">
-                          <svg
-                            version="1.0"
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="512.000000pt"
-                            height="512.000000pt"
-                            viewBox="0 0 512.000000 512.000000"
-                            preserveAspectRatio="xMidYMid meet"
-                          >
-                            <g
-                              transform="translate(0.000000,512.000000) scale(0.100000,-0.100000)"
-                              fill="#FFFFFF"
-                              stroke="none"
-                            >
-                              <path
-                                d="M1387 5110 c-243 -62 -373 -329 -272 -560 27 -62 77 -114 989 -1027
-l961 -963 -961 -963 c-912 -913 -962 -965 -989 -1027 -40 -91 -46 -200 -15
--289 39 -117 106 -191 220 -245 59 -28 74 -31 160 -30 74 0 108 5 155 23 58
-22 106 70 1198 1160 1304 1302 1202 1185 1202 1371 0 186 102 69 -1202 1371
--1102 1101 -1140 1137 -1198 1159 -67 25 -189 34 -248 20z"
-                              />
-                            </g>
-                          </svg>
-                        </div>
-                      </Swiper>
+                      <div>{swiperComponent}</div>
                       <div className="swiper-post-custom-pagination"></div>
                     </figure>
                   ) : (
@@ -250,100 +254,7 @@ l961 -963 -961 -963 c-912 -913 -962 -965 -989 -1027 -40 -91 -46 -200 -15
                   )}
                   {categorySlider ? (
                     <figure className={cx('image-slider')}>
-                      <Swiper
-                        spaceBetween={30}
-                        effect={'fade'}
-                        loop={true}
-                        autoplay={{
-                          delay: 25000,
-                          disableOnInteraction: true,
-                        }}
-                        pagination={{
-                          clickable: true,
-                          el: '.swiper-post-custom-pagination',
-                          clickable: true,
-                          renderBullet: function (index, className) {
-                            return `<span key="${menuIndex[index]}" class="${className}"></span>`
-                          },
-                        }}
-                        navigation={{
-                          prevEl: '.swiper-custom-button-prev',
-                          nextEl: '.swiper-custom-button-next',
-                        }}
-                        modules={[EffectFade, Autoplay, Pagination, Navigation]}
-                        className="post-category-swiper"
-                      >
-                        {categorySlider?.map((image, index) => (
-                          <div className="post-swiper-slide">
-                            {image[0] && (
-                              <SwiperSlide key={index}>
-                                <Image
-                                  src={image[0]}
-                                  alt={'Slider Image' + index}
-                                  fill
-                                  sizes="100%"
-                                  priority
-                                />
-                                {image[1] && (
-                                  <figcaption className={'slide-caption'}>
-                                    <span className={'caption'}>
-                                      {image[1]}
-                                    </span>
-                                  </figcaption>
-                                )}
-                              </SwiperSlide>
-                            )}
-                          </div>
-                        ))}
-                        <div className="swiper-custom-button-prev">
-                          <svg
-                            version="1.0"
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="512.000000pt"
-                            height="512.000000pt"
-                            viewBox="0 0 512.000000 512.000000"
-                            preserveAspectRatio="xMidYMid meet"
-                          >
-                            <g
-                              transform="translate(0.000000,512.000000) scale(0.100000,-0.100000)"
-                              fill="#FFFFFF"
-                              stroke="none"
-                            >
-                              <path
-                                d="M1387 5110 c-243 -62 -373 -329 -272 -560 27 -62 77 -114 989 -1027
-l961 -963 -961 -963 c-912 -913 -962 -965 -989 -1027 -40 -91 -46 -200 -15
--289 39 -117 106 -191 220 -245 59 -28 74 -31 160 -30 74 0 108 5 155 23 58
-22 106 70 1198 1160 1304 1302 1202 1185 1202 1371 0 186 102 69 -1202 1371
--1102 1101 -1140 1137 -1198 1159 -67 25 -189 34 -248 20z"
-                              />
-                            </g>
-                          </svg>
-                        </div>
-                        <div className="swiper-custom-button-next">
-                          <svg
-                            version="1.0"
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="512.000000pt"
-                            height="512.000000pt"
-                            viewBox="0 0 512.000000 512.000000"
-                            preserveAspectRatio="xMidYMid meet"
-                          >
-                            <g
-                              transform="translate(0.000000,512.000000) scale(0.100000,-0.100000)"
-                              fill="#FFFFFF"
-                              stroke="none"
-                            >
-                              <path
-                                d="M1387 5110 c-243 -62 -373 -329 -272 -560 27 -62 77 -114 989 -1027
-l961 -963 -961 -963 c-912 -913 -962 -965 -989 -1027 -40 -91 -46 -200 -15
--289 39 -117 106 -191 220 -245 59 -28 74 -31 160 -30 74 0 108 5 155 23 58
-22 106 70 1198 1160 1304 1302 1202 1185 1202 1371 0 186 102 69 -1202 1371
--1102 1101 -1140 1137 -1198 1159 -67 25 -189 34 -248 20z"
-                              />
-                            </g>
-                          </svg>
-                        </div>
-                      </Swiper>
+                      <div>{swiperComponent}</div>
                       <div className="swiper-post-custom-pagination"></div>
                     </figure>
                   ) : (
