d7985ea63bc4ad7edfd65981682d92e3fd86c594
WEB 1 <web1@iMac.local>
2026-01-07 15:38:38 +0700

    modified single-cotributor updates

--- wp-templates/single-contributor.js
+++ wp-templates/single-contributor.js
@@ -343,7 +343,7 @@ export default function Component(props) {
                     }
                     width={500}
                     height={500}
-                    className="h-auto w-full rounded-full border object-cover"
+                    className="h-auto w-full border object-cover"
                     priority
                   />
                 </div>
