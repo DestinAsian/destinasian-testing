ab7c60bbfd66608f78855eeefd718327fc4f46f9
geoffreyihsan <geoffreymohammad@gmail.com>
2026-06-09 12:46:45 +0700

    Update Advertorial / Partner Content, Editorial, Honors Circle, and Luxury Travel designs, modified Header, SecondaryHeader, link hover & focus style on All Pages

--- components/SingleSlider/SingleSlider.js
+++ components/SingleSlider/SingleSlider.js
@@ -1,5 +1,7 @@
-import React, { useMemo } from 'react'
+import React, { useState, useEffect, useId, useMemo } from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react'
+import className from 'classnames/bind'
+import styles from './SingleSlider.module.scss'
 
 import 'swiper/css'
 import 'swiper/css/effect-fade'
@@ -9,7 +11,16 @@ import 'swiper/css/navigation'
 import { EffectFade, Autoplay, Pagination, Navigation } from 'swiper'
 import Image from 'next/image'
 
+let cx = className.bind(styles)
+
 export default function SingleSlider({ images = [] }) {
+  const reactId = useId()
+
+  const paginationClass = useMemo(() => {
+    const safeId = reactId.replace(/[^a-zA-Z0-9_-]/g, '')
+    return `gallery-slider-pagination-${safeId}`
+  }, [reactId])
+
   const slides = useMemo(() => {
     if (!Array.isArray(images)) return []
 
@@ -54,89 +65,50 @@ export default function SingleSlider({ images = [] }) {
   if (!slides.length) return null
 
   return (
-    <>
-      <Swiper
-        spaceBetween={30}
-        effect="fade"
-        loop={slides.length > 1}
-        autoplay={{
-          delay: 25000,
-          disableOnInteraction: true,
-        }}
-        pagination={{
-          el: '.swiper-post-custom-pagination',
-          clickable: true,
-          renderBullet: function (index, className) {
-            return `<span key="${menuIndex[index]}" class="${className}"></span>`
-          },
-        }}
-        navigation={{
-          prevEl: '.swiper-custom-button-prev',
-          nextEl: '.swiper-custom-button-next',
-        }}
-        modules={[EffectFade, Autoplay, Pagination, Navigation]}
-        className="post-swiper"
-      >
-        {slides.map((slide, index) => (
-          <SwiperSlide key={index}>
-            <figure className="post-swiper-slide">
-              <Image
-                src={slide.src}
-                alt={slide.caption || `Slider Image ${index + 1}`}
-                fill
-                sizes="100%"
-                priority
-                quality={100}
-              />
-              {slide.caption && (
-                <figcaption className="slide-caption">
-                  {slide.caption}
-                </figcaption>
-              )}
-            </figure>
-          </SwiperSlide>
-        ))}
+    <div className={cx('component', 'gallery-slider-wrapper')}>
+      <div className={cx('swiper-slider', 'swiper-wrapper')}>
+        <Swiper
+          spaceBetween={30}
+          effect="fade"
+          autoplay={{
+            delay: 5000,
+            disableOnInteraction: true,
+          }}
+          loop={true}
+          pagination={{
+            el: `.${paginationClass}`,
+            clickable: true,
+            type: 'bullets',
+          }}
+          modules={[EffectFade, Autoplay, Pagination, Navigation]}
+          className="post-swiper"
+        >
+          {slides.map((slide, index) => (
+            <SwiperSlide key={index}>
+              <div className={cx('slide-wrapper', className)}>
+                <div className={cx('image-wrapper')}>
+                  <Image
+                    src={slide.src}
+                    alt={slide.caption || `Slider Image ${index + 1}`}
+                    fill
+                    sizes="100%"
+                    priority
+                    quality={100}
+                  />
+                  {slide.caption && (
+                    <figcaption className="slide-caption">
+                      {slide.caption}
+                    </figcaption>
+                  )}
+                </div>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
 
-        <div className="swiper-custom-button-prev">
-          <svg
-            version="1.0"
-            xmlns="https://www.w3.org/2000/svg"
-            width="512.000000pt"
-            height="512.000000pt"
-            viewBox="0 0 512.000000 512.000000"
-            preserveAspectRatio="xMidYMid meet"
-          >
-            <g
-              transform="translate(0.000000,512.000000) scale(0.100000,-0.100000)"
-              fill="#FFFFFF"
-              stroke="none"
-            >
-              <path d="M1387 5110 c-243 -62 -373 -329 -272 -560 27 -62 77 -114 989 -1027 l961 -963 -961 -963 c-912 -913 -962 -965 -989 -1027 -40 -91 -46 -200 -15 -289 39 -117 106 -191 220 -245 59 -28 74 -31 160 -30 74 0 108 5 155 23 58 22 106 70 1198 1160 1304 1302 1202 1185 1202 1371 0 186 102 69 -1202 1371 -1102 1101 -1140 1137 -1198 1159 -67 25 -189 34 -248 20z" />
-            </g>
-          </svg>
-        </div>
-
-        <div className="swiper-custom-button-next">
-          <svg
-            version="1.0"
-            xmlns="https://www.w3.org/2000/svg"
-            width="512.000000pt"
-            height="512.000000pt"
-            viewBox="0 0 512.000000 512.000000"
-            preserveAspectRatio="xMidYMid meet"
-          >
-            <g
-              transform="translate(0.000000,512.000000) scale(0.100000,-0.100000)"
-              fill="#FFFFFF"
-              stroke="none"
-            >
-              <path d="M1387 5110 c-243 -62 -373 -329 -272 -560 27 -62 77 -114 989 -1027 l961 -963 -961 -963 c-912 -913 -962 -965 -989 -1027 -40 -91 -46 -200 -15 -289 39 -117 106 -191 220 -245 59 -28 74 -31 160 -30 74 0 108 5 155 23 58 22 106 70 1198 1160 1304 1302 1202 1185 1202 1371 0 186 102 69 -1202 1371 -1102 1101 -1140 1137 -1198 1159 -67 25 -189 34 -248 20z" />
-            </g>
-          </svg>
-        </div>
-      </Swiper>
-
-      <div className="swiper-post-custom-pagination"></div>
-    </>
+        <div className={`swiper-post-custom-pagination ${paginationClass}`} />
+      </div>
+      <div className={cx('border-bottom')} />
+    </div>
   )
 }
