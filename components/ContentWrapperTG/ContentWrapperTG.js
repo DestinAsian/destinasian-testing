48c2506a6fb02ee9d197b8aae931c7a9a536484d
geoffreyihsan <geoffreymohammad@gmail.com>
2026-06-17 10:28:58 +0700

    modified ContentWrapperTG

--- components/ContentWrapperTG/ContentWrapperTG.js
+++ components/ContentWrapperTG/ContentWrapperTG.js
@@ -164,7 +164,15 @@ export default function ContentWrapperTG({
   }
 
   if (error) {
-    return <pre>{typeof error !== 'undefined' && error ? (error.message ? error.message : JSON.stringify(error)) : 'Unknown error'}</pre>
+    return (
+      <pre>
+        {typeof error !== 'undefined' && error
+          ? error.message
+            ? error.message
+            : JSON.stringify(error)
+          : 'Unknown error'}
+      </pre>
+    )
   }
 
   return (
@@ -208,80 +216,128 @@ export default function ContentWrapperTG({
         />
         <div
           style={{
-            marginLeft: !isMobile ? `${sliderWidth}px` : '',
-            width: !isMobile ? `calc( 100vw - ${sliderWidth}px)` : '',
+            // marginLeft: !isMobile ? `${sliderWidth}px` : '',
+            width: !isMobile ? '100vw' : '',
           }}
           className={cx('navigation-wrapper')}
         >
-          <div className={cx('navigation-button')}>
-            {prevUri && (
-              <Link href={prevUri} className={cx('prev-button')}>
-                <svg
-                  width="60"
-                  height="90"
-                  viewBox="0 0 60 90"
-                  fill="none"
-                  xmlns="https://www.w3.org/2000/svg"
-                >
-                  <path d="M52 8L15 45L52 82" stroke="none" strokeWidth="20" />
-                </svg>
-              </Link>
-            )}
-          </div>
-          <div className={cx('share-button-wrapper')}>
-            <div className={cx('image-wrapper')}>
-              <div className={cx('menu-button')}>
-                <button
-                  type="button"
-                  className={cx('share-icon')}
-                  onClick={handleShare}
-                  disabled={isSharing}
-                >
+          <div className={cx('navigation-inner-wrapper')}>
+            <div className={cx('navigation-button')}>
+              {prevUri && (
+                <Link href={prevUri} className={cx('prev-button')}>
                   <svg
+                    width="60"
+                    height="90"
+                    viewBox="0 0 60 90"
+                    fill="none"
                     xmlns="https://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    fill="currentColor"
-                    className="size-6"
                   >
                     <path
-                      fillRule="evenodd"
-                      d="M15.75 4.5a3 3 0 1 1 .825 2.066l-8.421 4.679a3.002 3.002 0 0 1 0 1.51l8.421 4.679a3 3 0 1 1-.729 1.31l-8.421-4.678a3 3 0 1 1 0-4.132l8.421-4.679a3 3 0 0 1-.096-.755Z"
-                      clipRule="evenodd"
+                      d="M52 8L15 45L52 82"
+                      stroke="none"
+                      strokeWidth="20"
                     />
                   </svg>
-                </button>
+                </Link>
+              )}
+            </div>
+            <div className={cx('share-button-wrapper')}>
+              <div className={cx('image-wrapper')}>
+                <div className={cx('menu-button')}>
+                  <button
+                    type="button"
+                    className={cx('share-icon')}
+                    onClick={handleShare}
+                    disabled={isSharing}
+                  >
+                    <svg
+                      xmlns="https://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      fill="currentColor"
+                      className="size-6"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M15.75 4.5a3 3 0 1 1 .825 2.066l-8.421 4.679a3.002 3.002 0 0 1 0 1.51l8.421 4.679a3 3 0 1 1-.729 1.31l-8.421-4.678a3 3 0 1 1 0-4.132l8.421-4.679a3 3 0 0 1-.096-.755Z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  </button>
+                </div>
               </div>
             </div>
-          </div>
-          <div className={cx('menu-center-wrapper')}>
-            {!isTGNavShown ? (
-              <div className={cx('image-wrapper')}>
-                {/* Menu Button */}
-                {isTGNavShown == false ? (
-                  <div className={cx('menu-button')}>
-                    {/* menu button */}
-                    <button
-                      type="button"
-                      className={cx('menu-icon')}
-                      onClick={() => {
-                        setIsTGNavShown(!isTGNavShown) // Toggles navigation visibility
-                        // if (isAutoplayRunning) {
-                        //   return toggleAutoplay() // Calls the toggleAutoplay function
-                        // }
-                      }}
-                      aria-controls={cx('full-menu-wrapper')}
-                      aria-expanded={!isTGNavShown}
-                    >
-                      {mainLogo && (
-                        <FeaturedImage
-                          image={mainLogo}
-                          className={cx('image')}
-                          priority
-                        />
-                      )}
-                    </button>
-                  </div>
-                ) : (
+            <div className={cx('menu-center-wrapper')}>
+              {!isTGNavShown ? (
+                <div className={cx('image-wrapper')}>
+                  {/* Menu Button */}
+                  {isTGNavShown == false ? (
+                    <div className={cx('menu-button')}>
+                      {/* menu button */}
+                      <button
+                        type="button"
+                        className={cx('menu-icon')}
+                        onClick={() => {
+                          setIsTGNavShown(!isTGNavShown) // Toggles navigation visibility
+                          // if (isAutoplayRunning) {
+                          //   return toggleAutoplay() // Calls the toggleAutoplay function
+                          // }
+                        }}
+                        aria-controls={cx('full-menu-wrapper')}
+                        aria-expanded={!isTGNavShown}
+                      >
+                        {mainLogo && (
+                          <FeaturedImage
+                            image={mainLogo}
+                            className={cx('image')}
+                            priority
+                          />
+                        )}
+                      </button>
+                    </div>
+                  ) : (
+                    <div className={cx('menu-button')}>
+                      {/* close button */}
+                      <button
+                        type="button"
+                        className={cx('close-icon')}
+                        onClick={() => {
+                          setIsTGNavShown(!isTGNavShown)
+                        }}
+                        aria-controls={cx('full-menu-wrapper')}
+                        aria-expanded={!isTGNavShown}
+                      >
+                        <svg
+                          version="1.0"
+                          xmlns="https://www.w3.org/2000/svg"
+                          width="512.000000pt"
+                          height="512.000000pt"
+                          viewBox="0 0 512.000000 512.000000"
+                          preserveAspectRatio="xMidYMid meet"
+                        >
+                          <g
+                            transform="translate(0.000000,512.000000) scale(0.100000,-0.100000)"
+                            fill="#000000"
+                            stroke="none"
+                          >
+                            <path
+                              d="M2330 5109 c-305 -29 -646 -126 -910 -259 -273 -138 -559 -356 -755
+-576 -384 -432 -602 -931 -655 -1499 -41 -446 55 -949 260 -1355 138 -273 356
+-559 576 -755 432 -384 931 -602 1499 -655 446 -41 949 55 1355 260 273 138
+559 356 755 576 384 432 602 931 655 1499 41 446 -55 949 -260 1355 -138 273
+-356 559 -576 755 -432 384 -931 602 -1499 655 -125 11 -320 11 -445 -1z
+m-193 -1701 l423 -423 425 425 425 425 212 -213 213 -212 -425 -425 -425 -425
+425 -425 425 -425 -213 -212 -212 -213 -425 425 -425 425 -425 -425 -425 -425
+-212 213 -213 212 425 425 425 425 -425 425 -425 425 210 210 c115 115 212
+210 215 210 3 0 195 -190 427 -422z"
+                            />
+                          </g>
+                        </svg>
+                      </button>
+                    </div>
+                  )}
+                </div>
+              ) : (
+                <div className={cx('image-wrapper')}>
                   <div className={cx('menu-button')}>
                     {/* close button */}
                     <button
@@ -321,70 +377,28 @@ m-193 -1701 l423 -423 425 425 425 425 212 -213 213 -212 -425 -425 -425 -425
                       </svg>
                     </button>
                   </div>
-                )}
-              </div>
-            ) : (
-              <div className={cx('image-wrapper')}>
-                <div className={cx('menu-button')}>
-                  {/* close button */}
-                  <button
-                    type="button"
-                    className={cx('close-icon')}
-                    onClick={() => {
-                      setIsTGNavShown(!isTGNavShown)
-                    }}
-                    aria-controls={cx('full-menu-wrapper')}
-                    aria-expanded={!isTGNavShown}
-                  >
-                    <svg
-                      version="1.0"
-                      xmlns="https://www.w3.org/2000/svg"
-                      width="512.000000pt"
-                      height="512.000000pt"
-                      viewBox="0 0 512.000000 512.000000"
-                      preserveAspectRatio="xMidYMid meet"
-                    >
-                      <g
-                        transform="translate(0.000000,512.000000) scale(0.100000,-0.100000)"
-                        fill="#000000"
-                        stroke="none"
-                      >
-                        <path
-                          d="M2330 5109 c-305 -29 -646 -126 -910 -259 -273 -138 -559 -356 -755
--576 -384 -432 -602 -931 -655 -1499 -41 -446 55 -949 260 -1355 138 -273 356
--559 576 -755 432 -384 931 -602 1499 -655 446 -41 949 55 1355 260 273 138
-559 356 755 576 384 432 602 931 655 1499 41 446 -55 949 -260 1355 -138 273
--356 559 -576 755 -432 384 -931 602 -1499 655 -125 11 -320 11 -445 -1z
-m-193 -1701 l423 -423 425 425 425 425 212 -213 213 -212 -425 -425 -425 -425
-425 -425 425 -425 -213 -212 -212 -213 -425 425 -425 425 -425 -425 -425 -425
--212 213 -213 212 425 425 425 425 -425 425 -425 425 210 210 c115 115 212
-210 215 210 3 0 195 -190 427 -422z"
-                        />
-                      </g>
-                    </svg>
-                  </button>
                 </div>
-              </div>
-            )}
-          </div>
-          <div className={cx('navigation-button')}>
-            {nextUri && (
-              <Link href={nextUri} className={cx('next-button')}>
-                <svg
-                  width="60"
-                  height="90"
-                  viewBox="0 0 60 90"
-                  fill="none"
-                  xmlns="https://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M8 82L45 45L8.00001 8"
-                    stroke="none"
-                    strokeWidth="20"
-                  />
-                </svg>
-              </Link>
-            )}
+              )}
+            </div>
+            <div className={cx('navigation-button')}>
+              {nextUri && (
+                <Link href={nextUri} className={cx('next-button')}>
+                  <svg
+                    width="60"
+                    height="90"
+                    viewBox="0 0 60 90"
+                    fill="none"
+                    xmlns="https://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M8 82L45 45L8.00001 8"
+                      stroke="none"
+                      strokeWidth="20"
+                    />
+                  </svg>
+                </Link>
+              )}
+            </div>
           </div>
         </div>
       </div>
