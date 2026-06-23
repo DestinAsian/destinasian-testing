3ac8b1651e9c01ea662b8be810836446317568ef
geoffreyihsan <geoffreymohammad@gmail.com>
2026-06-23 12:53:43 +0700

    modified RelatedStories

--- components/AdvertorialRelatedStories/AdvertorialRelatedStories.js
+++ components/AdvertorialRelatedStories/AdvertorialRelatedStories.js
@@ -164,7 +164,7 @@ export default function PageRelatedStories(databaseId) {
                           />
                         </div>
                       )}
-                      {post?.contentTypeName === 'travel-guide' && (
+                      {post?.contentTypeName === 'travel-guides' && (
                         <div className={cx('post-wrapper')}>
                           {/* Travel Guides Stories */}
                           <RelatedStories
